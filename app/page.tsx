eca0f3a826083bd4ef7a3d4319e225306f479ad6
bohdanlazurenko <142092646+bohdanlazurenko@users.noreply.github.com>
2025-10-18 21:31:58 +0200

    Initial commit for neuronix-task_8e2afb58

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
+
+export default function Home() {
+  return (
+    <main className="space-y-16">
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
+  )
+}
